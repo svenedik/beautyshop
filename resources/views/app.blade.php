6fc3d179a52cd994d8f89fd660c738d92182a535
salimcan venedik <salimcan@useinsider.com>
2018-05-25 09:30:08 +0300

    Logo & favicon added..

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,13 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{csrf_token()}}">
         <!-- Favicons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicons/apple-touch-icon.png')}}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicons/favicon-32x32.png')}}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicons/favicon-16x16.png')}}">
+        <link rel="manifest" href="{{asset('favicons/site.webmanifest')}}">
+        <link rel="mask-icon" href="{{asset('favicons/safari-pinned-tab.svg')}}" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
         @yield('aimeos_header')
         <!--@ yield('aimeos_styles')-->
         <!-- Styles -->
